668a4df20c18ef6e76fe47bb544b650e63f5a652
Rogerio Andrade <rogerioandrade.agroeng@gmail.com>
2026-05-17 18:38:27 -0300

    improve header and clean footer

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ export default function App() {
             <img
               src="/logo-territorial.png"
               alt="Territorial Engenharia"
-              className="h-16 w-auto"
+              className="h-30 w-auto"
             />
 
             <div>
